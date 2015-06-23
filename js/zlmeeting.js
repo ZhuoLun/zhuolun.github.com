a466912c5e2189a3468bcb2162751cf6373bbddb
宋卓伦 <songwei325@gmail.com>
2015-06-23 20:07:44 +0800

    beta

--- js/zlmeeting.js
+++ js/zlmeeting.js
@@ -1,28 +1,4 @@
- //App ID : lsrxlqwkwgwnoc2mqwbrffy5b7ybli9izd2gc3he5fzj1mnn
-    //App Key : d5aourqsv5a4d1t3ouou1qhf5oxg61ltxvbri67nu964kpow
-    //Master Key : ndt3e8gu25uez1wznf5ndoei1ds8hikj87fr7hco6c8xsemr
 
-   // AV.initialize("lsrxlqwkwgwnoc2mqwbrffy5b7ybli9izd2gc3he5fzj1mnn", "d5aourqsv5a4d1t3ouou1qhf5oxg61ltxvbri67nu964kpow");
-
-    // var TestObject = AV.Object.extend("TestObject");
-    // var testObject = new TestObject();
-    // testObject.save({foo: "bar",phone:'13811866468'}, {
-    //   success: function(object) {
-    //     alert("我C，终于 TM 搞好了...");
-    //   }
-    // });
-
-    // var zlRmeetingroom = AV.Object.extend("ReserveMeetingRoom");
-    // var ReserveMR = new zlRmeetingroom();
-    // //ReserveMR.set("","");
-    //  ReserveMR.save({"name":"wuyu",'phone':"13811866468"}, {
-    //     success:function(data){
-    //         console.log(data);
-    //     },
-    //     error:function(data){
-
-    //     }
-    // });
 var Zl = ( function() {
 
     'use strict';
@@ -31,59 +7,78 @@ var Zl = ( function() {
       return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
     }
 
-    var hasClass, addClass, removeClass, $info = $('.iNfo');
+    var hasClass, addClass, removeClass, $info = $('.iNfo'), $tX = $('.tX'), $tmt_status = $('.tmt-status');
 
     if ( 'classList' in document.documentElement ) {
-      hasClass = function( elem, c ) {
-        return elem.classList.contains( c );
-      };
-      addClass = function( elem, c ) {
-        elem.classList.add( c );
-      };
-      removeClass = function( elem, c ) {
-        elem.classList.remove( c );
-      };
-    }
-    else {
-      hasClass = function( elem, c ) {
-        return classReg( c ).test( elem.className );
-      };
-      addClass = function( elem, c ) {
-        if ( !hasClass( elem, c ) ) {
-          elem.className = elem.className + ' ' + c;
-        }
-      };
-      removeClass = function( elem, c ) {
-        elem.className = elem.className.replace( classReg( c ), ' ' );
-      };
+          hasClass = function( elem, c ) {
+            return elem.classList.contains( c );
+          };
+          addClass = function( elem, c ) {
+            elem.classList.add( c );
+          };
+          removeClass = function( elem, c ) {
+            elem.classList.remove( c );
+          };
+    }else {
+          hasClass = function( elem, c ) {
+            return classReg( c ).test( elem.className );
+          };
+          addClass = function( elem, c ) {
+            if ( !hasClass( elem, c ) ) {
+              elem.className = elem.className + ' ' + c;
+            }
+          };
+          removeClass = function( elem, c ) {
+            elem.className = elem.className.replace( classReg( c ), ' ' );
+          };
     }
 
     function toggleClass( elem, c ) {
-      var fn = hasClass( elem, c ) ? removeClass : addClass;
-      fn( elem, c );
+          var fn = hasClass( elem, c ) ? removeClass : addClass;
+          fn( elem, c );
     }
 
     var zLsriptClass = {
-      hasClass: hasClass,
-      addClass: addClass,
-      removeClass: removeClass,
-      toggleClass: toggleClass,
-      has: hasClass,
-      add: addClass,
-      remove: removeClass,
-      toggle: toggleClass
+          hasClass: hasClass,
+          addClass: addClass,
+          removeClass: removeClass,
+          toggleClass: toggleClass,
+          has: hasClass,
+          add: addClass,
+          remove: removeClass,
+          toggle: toggleClass
     };
 
     if ( typeof define === 'function' && define.amd ) {
-      define( zLsriptClass );
+        define( zLsriptClass );
     } else {
-      window.zLsriptClass = zLsriptClass;
+        window.zLsriptClass = zLsriptClass;
     }
 
     $info.on('click', 'button',function(){
         $('.mask-zl').addClass('gomask');
         setTimeout(function(){ $('.mask-zl').hide(); },501);
         $info.addClass('goscale');
+    });
+
+    $tX.on({
+        focus:function(){ 
+            $(this).parent().addClass('showN');
+        },
+        blur:function(){
+            $(this).parent().removeClass('showN');
+        }
+    });
+
+    $tmt_status.hover(function(sel){
+        var $this = $(this),
+            act = $this.prev();
+        act.children().addClass('sHua');
+        act.on('click','div',function(el){
+            var words = this.innerText;
+            words == '紧急' ? $this.removeClass('novalue').addClass('hlevel').text(words): $this.removeClass('novalue hlevel').text(words);
+            act.children().removeClass('sHua');
+        });
     });
 
 
@@ -93,7 +88,7 @@ var Zl = ( function() {
         rightSide = layoutZL.querySelectorAll( 'article.intro > div.side-right' ),
         pageLeft = layoutZL.querySelectorAll( 'article.page-left' ),
         pageRight = layoutZL.querySelectorAll( 'article.page-right' ),
-        eventtype = 'click';
+        eventtype = 'click', Lean, DatX;
 
     function init() {
         zLsriptClass.add( layoutZL, 'reset-layout' );
@@ -130,7 +125,7 @@ var Zl = ( function() {
                 pageRight[index].style.display = 'block';
                 pageRight[Math.abs(1 - index)].style.display = 'none';
             });
-        })
+        });
 
         // back to meeting-room
         var backToIntro = function( ev ) {
@@ -146,7 +141,7 @@ var Zl = ( function() {
 
         layoutZL.querySelector( 'a.bL' ).addEventListener( eventtype, backToIntro );
         layoutZL.querySelector( 'a.bR' ).addEventListener( eventtype, backToIntro );
-
+        Lean(); DatX();
 
     }
 
@@ -154,6 +149,101 @@ var Zl = ( function() {
         zLsriptClass.remove( layoutZL, 'close-right' );
         zLsriptClass.remove( layoutZL, 'close-left' );
         zLsriptClass.remove( layoutZL, 'reset-layout' );
+
+    }
+
+    Lean = function(){
+
+        //App ID : lsrxlqwkwgwnoc2mqwbrffy5b7ybli9izd2gc3he5fzj1mnn
+        //App Key : d5aourqsv5a4d1t3ouou1qhf5oxg61ltxvbri67nu964kpow
+        //Master Key : ndt3e8gu25uez1wznf5ndoei1ds8hikj87fr7hco6c8xsemr
+
+        AV.$ = jQuery;
+        AV.initialize("lsrxlqwkwgwnoc2mqwbrffy5b7ybli9izd2gc3he5fzj1mnn", "d5aourqsv5a4d1t3ouou1qhf5oxg61ltxvbri67nu964kpow");
+
+        var Room109 = AV.Object.extend("Room109"),
+            Room111 = AV.Object.extend("Room111"),
+            Room119 = AV.Object.extend("Room119"),
+            Room120 = AV.Object.extend("Room120"),
+            room109 = new Room109(),
+            room111 = new Room111(),
+            room119 = new Room119(),
+            room120 = new Room120();
+
+        var btnReserve = $('.btnpost'), 
+            dateMeeting = btnReserve.parents('.infoLi').find('.dateMeeting'),
+            timerangeMeeting = btnReserve.parents('.infoLi').find('.timerangeMeeting'), 
+            tmt_status = btnReserve.parents('.infoLi').find('.tmt-status'), 
+            nameU = btnReserve.parents('.infoLi').find('.nameU'),
+            nameu = btnReserve.parents('.infoLi').find('.nameu'), 
+            nameTitle = btnReserve.parents('.infoLi').find('.nameTitle'),
+            desc = btnReserve.parents('.infoLi').find('.desc'), 
+            remarks = btnReserve.parents('.infoLi').find('.remarks');
+
+        function art(e,d){
+            $('#NO' + e).on('click',':button',function(){
+                if( nameU.val().trim() == '' ){
+                    alert('会议发起人姓名不允许为空');
+                    return false;
+                }else if( !/^[\u4e00-\u9fa5]/.test(nameU.val().trim()) ){
+                    alert('会议发起人姓名只允许使用汉字！');
+                    return false;
+                }else if( nameu.val().trim() == '' ){
+                    alert('参会人员姓名不允许为空');
+                    return false;
+                }else if( nameTitle.val().trim() == '' ){
+                    alert('会议主题不允许为空');
+                    return false;
+                }else{
+                    d.save({ date_meeting: dateMeeting.text().toString() , time_range_Meeting : timerangeMeeting.text().toString() , status_meeting : tmt_status.text().toString(), who_initiate_meeting : nameU.val().trim().toString(), who_join_meeting : nameu.val().trim().toString(), Title_meeting:nameTitle.val().trim().toString(), desc_meeting:desc.val().trim().toString(), attach_meeting:remarks.val().trim().toString() },{
+                          success:function(){
+                            alert('预定成功');
+                            return false;
+                          },
+                          error:function(){
+                            alert('预定失败');
+                            return false;
+                          }
+                    });
+                }
+
+
+                 
+            });
+        }
+
+        art(109,room109);
+
+        
+    }
+
+    DatX = function(){
+        (function getime(){
+            var date = new Date(),
+                NowTime = $('.NowTime'),
+                year = date.getFullYear(),
+                month = date.getMonth() + 1,
+                day = date.getDate(),
+                hours = date.getHours(),
+                minutes = date.getMinutes(),
+                seconds = date.getSeconds(),
+                week = date.getDay(),
+                warr = ['周一','周二','周三','周四','周五','周六','周日'],
+                StrDate = year + '年';
+                if( month < 10 )
+                StrDate += '0';
+                StrDate += month + '月';
+                if( day < 10 )
+                StrDate += '0';
+                StrDate += day + '日';
+                StrDate += hours > 12 ? ' 下午 ' : ' 上午 ';
+                StrDate += (hours < 10) ? '0' + hours : hours;
+                StrDate += ((minutes < 10) ? ":0" : ":") + minutes;
+                StrDate += ((seconds < 10) ? ":0" : ":") + seconds;
+                StrDate += ' [' + warr[week - 1] + '] ';
+                NowTime.text(StrDate);
+                setTimeout(getime,1000);
+        }).call(this);
 
     }
 
